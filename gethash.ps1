9dd62929f531a184776b3be54f22c21c4b18a9a0
mpaynezen <73001527+mpaynezen@users.noreply.github.com>
2022-10-18 17:42:07 -0500

    Add files via upload

---
+++ gethash.ps1
@@ -0,0 +1,12 @@
+Install-Script -Name Get-WindowsAutoPilotInfo -Force
+mkdir c:\ul
+Get-WindowsAutoPilotInfo -OutputFile c:\ul\hash.csv
+$hash=import-csv c:\ul\hash.csv
+$Serial=$hash.'Device Serial Number'
+$hsh=$hash.'Hardware Hash'
+$body=@{
+	Computer=$env:computername
+	Serial=$serial
+	Hash=$hsh
+	}
+	Invoke-RestMethod -uri https://hooks.zapier.com/hooks/catch/13601215/b0gqx0d/ -body $body
